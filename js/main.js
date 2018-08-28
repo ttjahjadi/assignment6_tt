e5b895a1caf17c163a6c9e38084da76d17f4cb2b
ttjahjadi93 <thomasTjahjadi93@gmail.com>
2018-08-28 20:40:17 +1000

    Made changes for the background to change

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
 const cities = ["NYC", "SF", "LA", "ATX", "SYD"];
+const body = document.querySelector('body');
+const selectBox = document.querySelector('#city-type');
 
 cities.forEach(function (city) { 
     const tag = document.createElement("option");
-    const tagCity = document.createTextNode(city);
+    const tagCity = document.createTextNode(city.toUpperCase());
     tag.appendChild(tagCity);
-    document.querySelector('#city-type').appendChild(tag);
+    selectBox.appendChild(tag);
     // document.querySelector('#city-type').innerHTML = "<option>" + city + "<option>";
  })
+
+
+//  $('#city-type').change(function () { 
+//     if (selectBox.value === 'NYC'){
+//         // body.style.backgroundImage = 'url(images/nyc.jpg)';
+//         body.classList.add('nyc');
+//     }
+    
+// })
+
+$('#city-type').change(function () {
+    // body.classList.remove();
+    $('body').removeClass();
+    cities.forEach(function (city) { 
+        if (selectBox.value === city.toUpperCase()) {
+            body.classList.add(city.toLowerCase());
+        }
+     })
+})
 
 
